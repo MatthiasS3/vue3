08695f9053fc15afdbbb11616611b957bb9858e7
Matthias Seeber <matthiasseeber@MacBook-Air.local>
2020-09-20 15:43:37 +0200

    End Commit

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <nav>
       <router-link to="/">
         <div class="navigation__logo">
-          Twooter
+          Twotter
         </div>
       </router-link>
-      <div class="navigation__user">
-        {{ state.user.username }}
+      <div class="navigation__user" v-if="user">
+        {{ user.username }}
       </div>
     </nav>
     <router-view />
@@ -15,19 +15,15 @@
 </template>
 
 <script>
-  import { reactive } from "vue";
-
+  import { useStore } from "vuex";
+  import { computed } from "vue";
   export default {
     name: "App",
     setup() {
-      const state = reactive({
-        user: {
-          username: "Matthias93"
-        }
-      });
-
+      const store = useStore();
+      const user = computed(() => store.state.User.user);
       return {
-        state
+        user
       };
     }
   };
