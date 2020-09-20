2a72a6ebdb275dd0d1d154a383c62247a8797e86
Matthias Seeber <matthiasseeber@MacBook-Air.local>
2020-09-20 15:09:28 +0200

    Commit 3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <nav>
+      <router-link to="/">
+        <div class="navigation__logo">
+          Twooter
+        </div>
+      </router-link>
+      <div class="navigation__user">
+        {{ state.user.username }}
+      </div>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { reactive } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    setup() {
+      const state = reactive({
+        user: {
+          username: "Matthias93"
+        }
+      });
+
+      return {
+        state
+      };
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    min-height: 100vh;
+    background-color: #f3f5fa;
+    nav {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 10px 5%;
+      background-color: deeppink;
+      color: white;
+      .navigation__logo {
+        font-weight: bold;
+        font-size: 24px;
+      }
+      .navigation__user {
+        font-weight: bold;
+      }
+    }
+  }
 </style>
